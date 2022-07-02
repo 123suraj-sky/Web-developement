3a58c5259c0b0dc500a4332476984236ae542e15
Suraj <yadavsurajkumar665@gmail.com>
2022-07-02 09:57:26 +0530

    jugad of postman done

--- Code With Harry/full stack/4 Node JS/Code/70-71_express/app.js
+++ Code With Harry/full stack/4 Node JS/Code/70-71_express/app.js
@@ -2,7 +2,8 @@
 /*
 1) It is a web application framework of node js.
 2) It will do url parsel for us and help us routing
-3) It wil be scalebility*/
+3) It wil be scalebility
+*/
 
 
 const express = require("express");
@@ -13,7 +14,22 @@ const app = express();
 /* when user will send request to our server it will be 
 handled by this function */
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("This is homepage of my first express app");
+});
+
+app.get("/about", (req, res) => {
+    // res.send("This is about page via get");
+
+    // we can also send status code
+    res.status(200).send("This is about page via get");
+});
+
+app.get("/this", (req, res) => {
+    res.status(400).send("This page is not found");
+});
+
+app.post("/about", (req, res) => {
+    res.send("This is about page via posts via posts");
 });
 
 /* we have to tell on which port we want to listen */
