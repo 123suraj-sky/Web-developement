fdb6061bfb7d4d4e452de304ff8153ec74efed32
Suraj <yadavsurajkumar665@gmail.com>
2022-07-05 19:18:28 +0530

    project 3 - dance project started

---
+++ Code With Harry/full stack/4 Node JS/Code/75_danceWebsite/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80;
+
+//* Express Specific Stuff
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+//* Pug Specific Stuff
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+//* Endpoints
+app.get('/', (req, res) => {
+    const params = {};
+    res.status(200).render('index.pug', params);
+});
+
+//* Start The Server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
