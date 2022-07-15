159e03aef5734452a836c7fbd37d4d9f9db69802
Suraj <yadavsurajkumar665@gmail.com>
2022-07-15 20:20:25 +0530

    new js playlist from code with harry started

---
+++ YouTube/Code With Harry/HTML CSS JS/JS new - complete playlist/1_intro.js
@@ -0,0 +1,10 @@
+// First install node for that b/c we are using node for JS
+
+console.log("Hello World");
+
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+let c = addThreeNumbers(1, 2, 52);
+console.log(c);
